94317178cb0fc9f33e7d8f7b514711dd147f2e9f
runner <runner@Mac-1611844647949.local>
2021-01-28 14:43:15 +0000

    release/0.4.0

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "ZendeskSDKStorage",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(
+            name: "ZendeskSDKStorage",
+            targets: [
+                "ZendeskSDKStorageTargets"
+            ]
+        )
+    ],
+    dependencies: [
+        .package(name: "ZendeskSDKLogger",
+                 url: "https://github.com/zendesk/sdk_logger_ios",
+                 from: "0.4.0")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ZendeskSDKStorage",
+            path: "ZendeskSDKStorage.xcframework"
+        ),
+        .target(name: "ZendeskSDKStorageTargets",
+                dependencies: [
+                    .target(name: "ZendeskSDKStorage"),
+                    .product(name: "ZendeskSDKLogger", package: "ZendeskSDKLogger")
+                ],
+                path: "Sources")
+    ]
+)
+
